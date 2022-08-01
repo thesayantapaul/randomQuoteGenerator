2b599d4c735475dc6406373b434ab1416f313f24
sayanta paul <sayanta.cs@gmail.com>
2022-08-01 14:37:18 +0530

    updated

--- script.js
+++ script.js
@@ -5,28 +5,44 @@ sountBtn = document.querySelector(".sound"),
 copyBtn = document.querySelector(".copy"),
 twitterBtn = document.querySelector(".twitter");
 
-// random quote function
+// random quote generator function
 function randomQuote(){
     quoteBtn.classList.add("loading");
     quoteBtn.innerText = "loading..";
+    // fetching data from a random api
     fetch("https://free-quotes-api.herokuapp.com/").then(res => res.json()).then(result => {
-        console.log(result)
         quoteText.innerText = result.quote;
         authorName.innerText = result.author;
         quoteBtn.innerText = "New Quote";
         quoteBtn.classList.remove("loading");
-    
-    
     });
 }
 
 
 
-
+// for listening the quote
 sountBtn.addEventListener("click", ()=>{
-    let utterance = new SpeechSynthesisUtterance();
+    let utter = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+    utter.lang = 'en-IN';
+    utter.pitch = 1.1;
+    utter.volume = 2;
+    speechSynthesis.speak(utter);
 } );
 
+
+// for copying the quote
+copyBtn.addEventListener("click", ()=>{
+   alert("Copied");
+   navigator.clipboard.writeText(quoteText.innerText);
+} );
+
+// for tweeting the quote
+twitterBtn.addEventListener("click", ()=>{
+   let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+   window.open(tweetUrl, "_blank");
+ } );
+
+ 
 quoteBtn.addEventListener("click", randomQuote);
 
 
